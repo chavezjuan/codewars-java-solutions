cafd560e540cb74960bb6a1423108ec9f510ab11
chavezdev <jctecenge@gmail.com>
2021-01-16 21:21:21 -0300

    EvenOrOdd unit tests

--- codewars/src/test/java/com/javasolutions/codewars/codewars/CodewarsApplicationTests.java
+++ codewars/src/test/java/com/javasolutions/codewars/codewars/CodewarsApplicationTests.java
@@ -9,9 +9,13 @@ class CodewarsApplicationTests {
 
 	@Test
 	public void checkIfZeroShouldReturnEven() {
-		EvenOrOdd tester = new EvenOrOdd();
+		assertEquals("Even", EvenOrOdd.even_or_odd(0), "0 number must be even");
+	}
 
-		assertEquals("Even", tester.even_or_odd(0), "0 number must be even");
+	@Test
+	public void testEvenOrOdd() {
+		assertEquals("Even", EvenOrOdd.even_or_odd(6), "Even number must be even");
+		assertEquals("Odd", EvenOrOdd.even_or_odd(7), "Odd number must be odd");
 	}
 
 }
